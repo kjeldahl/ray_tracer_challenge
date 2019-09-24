fce9f177384c0d38246b5beb4a42eb2be99124e7
Jacob Kjeldahl <jacob@kjeldahl.it>
2019-09-24 16:30:21 +0200

    Adds view transformation

--- features/step_definitions/transformaton_steps.rb
+++ features/step_definitions/transformaton_steps.rb
@@ -10,10 +10,23 @@ Given("{var} ← {scaling}") do |var, scaling|
   set(var, scaling)
 end
 
-And("{var} ← {rotation}") do |var, rotation|
+Given("{var} ← {rotation}") do |var, rotation|
   set(var, rotation)
 end
 
 Given("{var} ← {shearing}") do |var, shearing|
   set(var, shearing)
 end
+
+Given("{var} ← view_transform<{var}, {var}, {var}>") do |var, from, to, up|
+  set(var, MyMatrix.view_transform(get(from), get(to), get(up)))
+end
+
+Then("{var} = {scaling}") do |var, scaling|
+  expect(get(var)).to eq scaling
+end
+
+Then("{var} = {translation}") do |var, translation|
+  expect(get(var)).to eq translation
+end
+
--- lib/my_matrix.rb
+++ lib/my_matrix.rb
@@ -86,6 +86,21 @@ class MyMatrix
       ])
     end
 
+    def view_transform(from, to, up)
+      forward = (to - from).normalize
+      left = forward.cross(up.normalize)
+      true_up = left.cross(forward)
+
+      view =
+        MyMatrix.new(4, 4, [
+          [    left.x,     left.y,     left.z, 0.0],
+          [ true_up.x,  true_up.y,  true_up.z, 0.0],
+          [-forward.x, -forward.y, -forward.z, 0.0],
+          [       0.0,        0.0,        0.0, 1.0]
+        ])
+
+      view * translation(-from.x, -from.y, -from.z)
+    end
     # endregion
 
     # region Consistent Transformation naming with instances
