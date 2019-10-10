7b6ed78d56a47d27525d35a3d8625d47cb83e203
Jacob Kjeldahl <jacob@kjeldahl.it>
2019-10-10 14:55:52 +0200

    Implements fir_branch

--- lib/shapes/composite/fir_branch.rb
+++ lib/shapes/composite/fir_branch.rb
@@ -20,14 +20,32 @@ class FirBranch
 
       max_length = 20.0 * radius
 
-      object = Group.new
-      object.add_child(branch)
+      Group.new(transform: transform, material: material).tap do |object|
+        object.add_child(branch)
 
-      (0...segments).each do |y|
-        subgroup = Group.new
-        (0...per_segment).each do |i|
+        (0...segments).each do |y|
+          subgroup = Group.new
+          object.add_child(subgroup)
+          (0...per_segment).each do |i|
+            y_base = seg_size * y + rand * seg_size
+            y_tip = y_base - rand * seg_size
 
+            y_angle = i * theta + rand * theta
+            needle_length = max_length / 2 * (1 + rand)
 
+            ofs = radius / 2
+
+            p1 = Tuple.point(ofs, y_base, ofs)
+            p2 = Tuple.point(-ofs, y_base, ofs)
+            p3 = Tuple.point(0.0, y_tip, needle_length)
+
+            tri = Triangle.new(p1, p2, p3,
+                               transform: MyMatrix.rotate(:y, y_angle),
+                               material: Material.new(color: Color.new(0.26, 0.36, 0.16),
+                                                      specular: 0.1))
+
+            subgroup.add_child(tri)
+          end
         end
       end
     end
