af1acffa1d61ae7d7bace581ccfdd8ddca1c1684
Jacob Kjeldahl <jacob@kjeldahl.it>
2019-10-14 23:46:56 +0200

    Adds bounds calculation for CSG

--- lib/shapes/csg.rb
+++ lib/shapes/csg.rb
@@ -1,8 +1,23 @@
 class CSG < Shape
 
-  attr_reader :left, :right, :operation
+  class << self
+    def intersection_allowed(op, lhit, inl, inr)
+      rhit = !lhit
+      case op
+        when :union
+          return (lhit && !inr) || (rhit && !inl)
+        when :intersection
+          return (lhit && inr) || (rhit && inl)
+        when :difference
+          return (lhit && !inr) || (rhit && inl)
+        else
+          raise "Unknown operation: #{op}"
+      end
 
-  # TODO: Need to implement bounds
+    end
+  end
+
+  attr_reader :left, :right, :operation
 
   def initialize(left, op, right, transform: MyMatrix.identity, material: Material.default, bounds_check: true, shadow: true)
     super(transform: transform, material: material, shadow: shadow)
@@ -37,30 +52,40 @@ class CSG < Shape
   end
 
   def local_intersect(local_ray)
-    left_i = @left.intersect(local_ray)
-    right_i = @right.intersect(local_ray)
-    filter_intersections(left_i.combine(right_i))
+    if @bounds_check
+      if bounds.local_intersect(local_ray).any?
+        CallStatistics.add(:csg_bounds_hit)
+        local_children_intersect(local_ray)
+      else
+        CallStatistics.add(:csg_bounds_miss)
+        Intersections.empty
+      end
+    else
+      CallStatistics.add(:csg_bounds_skipped)
+      local_children_intersect(local_ray)
+    end
   end
 
   def local_normal_at(local_point, intersection=nil)
     raise "Should not be called"
   end
 
+  # TODO: Could be smaller for difference and intersections
+  def bounds
+    return @bounds if @bounds
 
-  class << self
-    def intersection_allowed(op, lhit, inl, inr)
-      rhit = !lhit
-      case op
-        when :union
-          return (lhit && !inr) || (rhit && !inl)
-        when :intersection
-          return (lhit && inr) || (rhit && inl)
-        when :difference
-          return (lhit && !inr) || (rhit && inl)
-        else
-          raise "Unknown operation: #{op}"
-      end
-
+    child_bounds = [left, right].map do |child|
+      child.bounds.transform(child.transform)
     end
+    @bounds      = Bounds.bounds_of_bounds(*child_bounds)
   end
+
+  private
+
+    def local_children_intersect(local_ray)
+      left_i  = @left.intersect(local_ray)
+      right_i = @right.intersect(local_ray)
+      filter_intersections(left_i.combine(right_i))
+    end
 end
+
